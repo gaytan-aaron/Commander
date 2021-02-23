8cccb69b1615e672eb653974d030d8b457a34e39
gaytan-aaron <aaron.gaytan@comunidad.unam.mx>
2021-02-23 12:11:56 -0600

    Aplicacion con peticiones sencillas

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using Commander.Models;
+using System.Collections.Generic;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan"},
+                new Command { Id = 1, HowTo = "Cut breath", Line = "Get a knife", Platform = "knife & chopping board"},
+                new Command { Id = 2, HowTo = "Make cup of tea", Line = "Place teabag in cup", Platform = "Kettle & cup"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+        }
+    }
+}
